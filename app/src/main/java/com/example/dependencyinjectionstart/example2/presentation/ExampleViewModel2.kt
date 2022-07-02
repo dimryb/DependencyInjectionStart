155237a43d3392a9d73ecc22ad5235766af615de
dmitry <dimryb@bk.ru>
2022-07-02 18:01:39 +0700

    Заменил предыдущее на созданные квалификаторы

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
@@ -2,14 +2,16 @@ package com.example.dependencyinjectionstart.example2.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.dependencyinjectionstart.example2.di.IdQualifier
+import com.example.dependencyinjectionstart.example2.di.NameQualifier
 import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 import javax.inject.Named
 
 class ExampleViewModel2 @Inject constructor(
     private val useCase: ExampleUseCase,
-    @Named("id") private val id: String,
-    @Named("name") private val name: String
+    @IdQualifier private val id: String,
+    @NameQualifier private val name: String
 ) : ViewModel() {
 
     fun method() {
