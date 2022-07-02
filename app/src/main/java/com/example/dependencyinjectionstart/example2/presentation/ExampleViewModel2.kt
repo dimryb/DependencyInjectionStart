7ad1e965b4fd052e9c00caa6555dd145daa640ca
dmitry <dimryb@bk.ru>
2022-07-02 16:08:17 +0700

    Создал Subcomponent для передачи аргумента во ViewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
@@ -6,11 +6,12 @@ import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 
 class ExampleViewModel2 @Inject constructor(
-    private val useCase: ExampleUseCase
+    private val useCase: ExampleUseCase,
+    private val id: String
 ) : ViewModel() {
 
     fun method() {
         useCase()
-        Log.d("ExampleViewModel2", "$this")
+        Log.d("ExampleViewModel2", "$this $id")
     }
 }
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -6,7 +6,6 @@ import com.example.dependencyinjectionstart.example2.di.ApplicationScope
 import javax.inject.Inject
 import javax.inject.Provider
 
-@ApplicationScope
 class ViewModelFactory @Inject constructor(
     private val viewModelsProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
